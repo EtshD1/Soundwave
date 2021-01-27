76d945492e9fd55ab96c75ec320284d93c4dee9b
EtshD1 <H79Pain@Gmail.com>
2021-01-27 17:53:45 +0200

    Add images path

--- src/images.js
+++ src/images.js
@@ -1,9 +1,21 @@
 import Logo from "./images/Logo.svg";
 import Lady from "./images/landing-page-lady.png";
+import Albums from "./images/Albums.svg";
+import More from "./images/Arrow.svg";
+import Charts from "./images/Charts.svg";
+import Podcasts from "./images/Podcasts.svg";
+import DivideCover from "./images/DivideCover.jpg"
+import ForestHillDriveCover from "./images/ForestHillDriveCover.jpg"
+import PostTraumaticCover from "./images/PostTraumaticCover.jpg"
+import TrenchCover from "./images/TrenchCover.jpg"
 
 const images = {
   Logo,
-  Lady
+  Lady,
+  Albums,
+  More,
+  Charts,
+  Podcasts
 }
 
 export default images

2afdf78e8b712378ca154e0f9f6e8ceea3cdebdf
EtshD1 <H79Pain@Gmail.com>
2021-01-27 17:45:32 +0200

    style footer

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -4,22 +4,30 @@ import Styles from "./Styles/footer.module.css";
 export default function Footer(props) {
   return (
     <footer>
-      <div>
+      <div className={Styles.Container}>
         <div className={Styles.Links}>
-          <span href="#">
-            About Us
-          </span>
-          <span href="#">
-            Contact
-          </span>
+          <div>
+            <span>
+              About Us
+            </span>
+          </div>
+          <div>
+            <span>
+              Contact
+            </span>
+          </div>
         </div>
-        <div>
-          <span href="#">
-            Twitter
-          </span>
-          <span href="#">
-            Facebook
-          </span>
+        <div className={Styles.SocialLink}>
+          <div>
+            <span>
+              Twitter
+            </span>
+          </div>
+          <div>
+            <span>
+              Facebook
+            </span>
+          </div>
         </div>
       </div>
     </footer>
